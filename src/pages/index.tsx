17d62075f21a940f0942bd3bfb7acf59f1b34f6e
Pedro5722 <pedroalbuquerquearaujo@gmail.com>
2021-04-21 12:03:00 -0300

    Aula 2 -20/04/21
    Conceitos de Static Site Generation e importação de json para o front.
    
    Estilização do playerEmpty
    
    Criação dos botões do player

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,24 @@
-import { Header } from "../components/Header";
 
-export default function Home() {
+import { useEffect } from "react"
+export default function Home(props) {
   return (
-    <Header/>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
   
+}
+
+export async function getStaticProps(){
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return{
+    props: {
+      episodes: data,
+    },
+
+    revalidate: 60*60*8,
+  }
 }
