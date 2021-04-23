0418319337fb8f14e68c332d111b117bf20df5c5
Pedro5722 <pedroalbuquerquearaujo@gmail.com>
2021-04-22 21:15:19 -0300

    aula 4
    #NLW5
    Adiciona um player funcional no app

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -2,6 +2,7 @@ import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter} from 'next/router';
 import { GetStaticPaths, GetStaticProps} from 'next';
 import {convertDurationToTimeString} from '../../utils/convertDurationToTimeString';
 import { api } from '../../services/api'
@@ -57,9 +58,26 @@ export default function Episode( {episode}: EpisodeProps){
 }
 
 export const getStaticPaths : GetStaticPaths = async() => {
+    
+    const {data} = await api.get('episodes',{
+        params: { 
+          _limit: 2,
+          _sort: 'published_at',
+          _order: 'desc',
+        }
+      })
+    
+    const paths = data.map(episode =>{
+        return{
+            params:{
+                slug: episode.id,
+            }
+        }
+    })
+    
     return {
         paths: [],
-        fallback: 'blocking'
+        fallback: 'blocking',
     }
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useContext } from 'react';
 import { GetStaticProps } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -6,6 +7,7 @@ import ptBR from 'date-fns/locale/pt-BR';
 
 import { api } from '../services/api';
 import {convertDurationToTimeString} from '../utils/convertDurationToTimeString';
+import { PlayerContext } from '../contexts/playerContext';
 
 import styles from './home.module.scss';
 
@@ -14,7 +16,7 @@ type Episode = {
   title: string,
   thumbnail: string,
   members: string,
-  duration: Number,
+  duration: number,
   durationAsString: string,
   url: string,
   publishedAt: string,
@@ -26,6 +28,9 @@ type HomeProps = {
 }
 
 export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
+
+  const {play} = useContext(PlayerContext);
+ 
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
@@ -52,7 +57,7 @@ export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
                   <span>{episode.durationAsString}</span>                  
                 </div>
 
-                <button type="button">
+                <button type="button" onClick={() => play(episode)}>
                   <img src="/play-green.svg" alt="Tocar episódio"/>
                 </button>
               </li>
@@ -97,7 +102,7 @@ export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
                   <td style={{ width: 100 }}>{episode.publishedAt}</td>
                   <td>{episode.durationAsString}</td>
                   <td>
-                    <button type="button">
+                    <button type="button" onClick={() => play(episode)}>
                       <img src="/play-green.svg" alt="Tocar episódio"/>
                     </button>
                   </td>
